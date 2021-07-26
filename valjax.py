e77a16012e63d8e54c67b100ce2bbae73b82aca8
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-07-25 20:56:05 -0400

    simulation tools

--- valjax.py
+++ valjax.py
@@ -2,13 +2,15 @@ from math import prod
 from operator import mul
 from itertools import accumulate
 
-import jax.numpy as npx
+import jax.numpy as np
+from jax import tree_map
+from jax.lax import scan
 
 def get_strides(shape):
     return tuple(accumulate(shape[-1:0:-1], mul, initial=1))[::-1]
 
 # index: [N, K] matrix or tuple of K [N] vectors
-def ravel_index(index, shape, np=npx):
+def ravel_index(index, shape):
     idxmat = np.stack(index, axis=-1)
     stride = np.array(get_strides(shape))
     return np.dot(idxmat, stride)
@@ -19,12 +21,16 @@ def ensure_tuple(x):
     else:
         return x
 
+# classic bounded smoothstep
+def smoothstep(x):
+    return np.where(x > 0, np.where(x < 1, 3*x**2 - 2*x**3, 1), 0)
+
 # for each element in left space (N), take elements in right space (M)
 # includes bounds checking
 # x  : [N..., M...]
 # iv : tuple of len(M) [N...]
 # ret: [N...]
-def address0(x, iv, np=npx):
+def address0(x, iv):
     iv = ensure_tuple(iv)
 
     K = len(iv)
@@ -32,7 +38,7 @@ def address0(x, iv, np=npx):
     lN, lM = prod(sN), prod(sM)
 
     ic = [np.clip(i, 0, n-1) for i, n in zip(iv, sM)]
-    ix = lM*np.arange(lN) + ravel_index(ic, sM, np=np).flatten()
+    ix = lM*np.arange(lN) + ravel_index(ic, sM).flatten()
 
     y0 = np.take(x, ix)
     y = np.reshape(y0, sN)
@@ -40,7 +46,7 @@ def address0(x, iv, np=npx):
     return y
 
 # generalized axis
-def address(x, iv, axis, np=npx):
+def address(x, iv, axis):
     iv = ensure_tuple(iv)
     axis = ensure_tuple(axis)
 
@@ -50,12 +56,12 @@ def address(x, iv, axis, np=npx):
     xs = np.moveaxis(x, axis, end)
 
     # perform on last axis
-    y = address0(xs, iv, np=np)
+    y = address0(xs, iv)
 
     return y
 
 # multi-dimensional argmax
-def argmax(x, axis, np=npx):
+def argmax(x, axis):
     axis = ensure_tuple(axis)
 
     # shuffle axes to end
@@ -74,11 +80,11 @@ def argmax(x, axis, np=npx):
     return iv
 
 # get the smooth index of the maximum (quadratic)
-def smoothmax(x, axis, np=npx):
+def smoothmax(x, axis):
     axis = ensure_tuple(axis)
 
-    i0 = argmax(x, axis, np=np)
-    y0 = address(x, i0, axis, np=np)
+    i0 = argmax(x, axis)
+    y0 = address(x, i0, axis)
     ir = []
 
     for k, ax in enumerate(axis):
@@ -87,8 +93,8 @@ def smoothmax(x, axis, np=npx):
         im = [ix - 1 if j == k else ix for j, ix in enumerate(i0)]
         ip = [ix + 1 if j == k else ix for j, ix in enumerate(i0)]
 
-        ym = address(x, im, axis, np=np)
-        yp = address(x, ip, axis, np=np)
+        ym = address(x, im, axis)
+        yp = address(x, ip, axis)
 
         dm = y0 - ym
         dp = y0 - yp
@@ -103,7 +109,7 @@ def smoothmax(x, axis, np=npx):
 # x  : [N..., M...]
 # iv : tuple of len(M) [N...]
 # ret: [N...]
-def interp_address(y, iv, axis, extrap=True, np=npx):
+def interp_address(y, iv, axis, extrap=True):
     iv = ensure_tuple(iv)
     axis = ensure_tuple(axis)
     shape = [y.shape[a] for a in axis]
@@ -117,7 +123,7 @@ def interp_address(y, iv, axis, extrap=True, np=npx):
         iv = [np.clip(i, 0, n-1) for i, n in zip(iv, shape)]
         i0 = [np.floor(i).astype(np.int32) for i in iv]
 
-    y0 = address(y, i0, axis, np=np)
+    y0 = address(y, i0, axis)
 
     vr = y0
 
@@ -125,7 +131,7 @@ def interp_address(y, iv, axis, extrap=True, np=npx):
         i0k, ivk = i0[k], iv[k]
         i1 = [i0k + 1 if j == k else ix for j, ix in enumerate(i0)]
 
-        y1 = address(y, i1, axis, np=np)
+        y1 = address(y, i1, axis)
         vr += (ivk-i0k) * (y1-y0)
 
     return vr
@@ -133,7 +139,7 @@ def interp_address(y, iv, axis, extrap=True, np=npx):
 # akin to continuous array indexing
 # requires len(iv) == x.ndim
 # return shape is that of iv's
-def interp(x, iv, extrap=True, np=npx):
+def interp(x, iv, extrap=True):
     iv = ensure_tuple(iv)
 
     if extrap:
@@ -157,3 +163,22 @@ def interp(x, iv, extrap=True, np=npx):
         vr += (ivk-i0k) * (y1-y0)
 
     return vr
+
+# state-only iteration
+def iterate(func, st0, T, hist=True):
+    tvec = np.arange(T)
+    dub = lambda x, _: 2*(func(x),)
+    last, path = scan(dub, st0, tvec)
+    if hist:
+        return path
+    else:
+        return last
+
+# simulate from tree of differential maps
+def simdiff(func, st0, Δ, T, hist=True):
+    up = lambda x, d: x + Δ*d
+    def update(st):
+        dst = func(st)
+        stp = tree_map(up, st, dst)
+        return stp
+    return iterate(update, st0, T, hist=hist)
